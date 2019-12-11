3a60c9f18c3e43c596aefa5623bff5202e62c377
OlukaDenis <olukadeno@gmail.com>
2019-12-11 03:03:24 +0300

    Adde the questions controller plus the tests

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def new
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-
+  resources :questions
   root 'static_pages#home'
 end
