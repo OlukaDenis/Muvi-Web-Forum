b38e3309ba735fd6b8f84a58d489a1289d7ea0e0
OlukaDenis <olukadeno@gmail.com>
2019-12-11 04:35:03 +0300

    More app functionalities: 1. Added the question controller, model and views. 2. Updated the root route 3. Added the show, index, create methods to the question controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'questions#index', as: 'home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   resources :questions
-  root 'static_pages#home'
 end
