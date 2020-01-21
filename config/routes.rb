d2d971cbddf09582a63834b2b43908cd199ff374
OlukaDenis <olukadeno@gmail.com>
2020-01-21 20:27:21 +0300

    App enhancement 1. Added the user pprofile 2. Added the year picker in the footer 3. Hot fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
   root 'questions#index'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   resources :questions do
     resources :answers
   end
